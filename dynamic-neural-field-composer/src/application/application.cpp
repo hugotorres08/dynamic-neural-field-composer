6861d84978474e3839b5f83c6eefcbb6d6bd9496
joao cunha <gasparcunha19@outlook.pt>
2024-11-10 10:16:36 +0000

    adding imgui and implot demos to main window, changes to imgui style

--- dynamic-neural-field-composer/src/application/application.cpp
+++ dynamic-neural-field-composer/src/application/application.cpp
@@ -22,6 +22,17 @@ namespace dnf_composer
 		simulation->init();
 		gui->initialize();
 		log(tools::logger::LogLevel::INFO, "Application initialized successfully.");
+
+		// [Put this elsewhere] Enable Keyboard Controls
+		auto io = ImGui::GetIO();
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+		// [Put this elsewhere] Using the light style with borders
+		ImGui::StyleColorsLight();
+		ImGuiStyle& style = ImGui::GetStyle();
+		style.FrameBorderSize = 1.0f;
+		style.FrameRounding = 6.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 0.7f;
+		style.Colors[ImGuiCol_FrameBg].w = 0.8f;
 	}
 
 	void Application::step() const
@@ -62,7 +73,11 @@ namespace dnf_composer
 		using namespace imgui_kit;
 		const WindowParameters winParams{ "Dynamic Neural Field Composer" };
 		const FontParameters fontParams{ std::string(PROJECT_DIR) + "/resources/fonts/Lexend-Light.ttf", 15 };
-		const StyleParameters styleParams{ ImVec4(0.3f, 0.3f, 0.3f, 0.6f), colours::White };
+
+		//const StyleParameters styleParams{ ImVec4(0.3f, 0.3f, 0.3f, 0.6f), colours::White };
+		constexpr ImVec4 themeColor = ImVec4(0.55f, 0.80f, 0.85f, 0.85f);
+		const StyleParameters styleParams{ themeColor, colours::White };
+
 		const IconParameters iconParams{ std::string(PROJECT_DIR) + "/resources/icons/icon.ico" };
 		const BackgroundImageParameters bgParams{ std::string(PROJECT_DIR) + "/resources/images/background.png", ImageFitType::ZOOM_TO_FIT };
 		const UserInterfaceParameters guiParameters{ winParams, fontParams, styleParams, iconParams, bgParams };
